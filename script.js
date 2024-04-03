48065339d6e0c50af09642b0be7241f1266b0f61
Shamil kp <shamilkp7895@gmail.com>
2024-04-03 12:11:04 +0530

    irshad web java script and css file

---
+++ script.js
@@ -0,0 +1,96 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next(){
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}
+
+function prev(){
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
+}
+
+document.querySelectorAll('.featured-image-1').forEach(image_1 =>{
+  image_1.addEventListener('click', () =>{
+    var src = image_1.getAttribute('src');
+    document.querySelector('.big-image-1').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-2').forEach(image_2 =>{
+  image_2.addEventListener('click', () =>{
+    var src = image_2.getAttribute('src');
+    document.querySelector('.big-image-2').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-3').forEach(image_3 =>{
+  image_3.addEventListener('click', () =>{
+    var src = image_3.getAttribute('src');
+    document.querySelector('.big-image-3').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-4').forEach(image_4 =>{
+  image_4.addEventListener('click', () =>{
+    var src = image_4.getAttribute('src');
+    document.querySelector('.big-image-4').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-5').forEach(image_5 =>{
+  image_5.addEventListener('click', () =>{
+    var src = image_5.getAttribute('src');
+    document.querySelector('.big-image-5').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-6').forEach(image_6 =>{
+  image_6.addEventListener('click', () =>{
+    var src = image_6.getAttribute('src');
+    document.querySelector('.big-image-6').src = src;
+  });
+});
+
+let btn = document.getElementById("btn");
+let btnText = document.getElementById("btnText");
+let btnIcon = document.getElementById("btnIcon");
+
+btn.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+
+  if(document.body.classList.contains("dark-theme")){
+    btnIcon.src = "images/sun.png";
+    btnText.innerHTML = "Light";
+  }else {
+    btnIcon.src = "images/moon.png";
+    btnText.innerHTML = "Dark";
+  }
+}
+
+var noti = document.querySelector('h1');
+var select = document.querySelector('.select');
+var button = document.getElementsByTagName('button');
+for (but of button)
+{
+  but.addEventListener('click',(e)=>{
+    var add = Number(noti.getAttribute('data-count')|| 0);
+    noti.classList.add('zero')
+  })
+}
