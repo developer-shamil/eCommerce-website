fd574cd03a40130e89434f4e045179901f4c67db
Shamil kp <shamilkp7895@gmail.com>
2024-04-13 01:56:08 +0530

    product images not showing problem fix

--- script.js
+++ script.js
@@ -26,42 +26,42 @@ function prev(){
   slides[index].classList.add('active');
 }
 
-document.querySelectorAll('.featured-image-1').forEach(image_1 =>{
+document.querySelectorAll('.product-image-1').forEach(image_1 =>{
   image_1.addEventListener('click', () =>{
     var src = image_1.getAttribute('src');
     document.querySelector('.big-image-1').src = src;
   });
 });
 
-document.querySelectorAll('.featured-image-2').forEach(image_2 =>{
+document.querySelectorAll('.product-image-2').forEach(image_2 =>{
   image_2.addEventListener('click', () =>{
     var src = image_2.getAttribute('src');
     document.querySelector('.big-image-2').src = src;
   });
 });
 
-document.querySelectorAll('.featured-image-3').forEach(image_3 =>{
+document.querySelectorAll('.product-image-3').forEach(image_3 =>{
   image_3.addEventListener('click', () =>{
     var src = image_3.getAttribute('src');
     document.querySelector('.big-image-3').src = src;
   });
 });
 
-document.querySelectorAll('.featured-image-4').forEach(image_4 =>{
+document.querySelectorAll('.product-image-4').forEach(image_4 =>{
   image_4.addEventListener('click', () =>{
     var src = image_4.getAttribute('src');
     document.querySelector('.big-image-4').src = src;
   });
 });
 
-document.querySelectorAll('.featured-image-5').forEach(image_5 =>{
+document.querySelectorAll('.product-image-5').forEach(image_5 =>{
   image_5.addEventListener('click', () =>{
     var src = image_5.getAttribute('src');
     document.querySelector('.big-image-5').src = src;
   });
 });
 
-document.querySelectorAll('.featured-image-6').forEach(image_6 =>{
+document.querySelectorAll('.product-image-6').forEach(image_6 =>{
   image_6.addEventListener('click', () =>{
     var src = image_6.getAttribute('src');
     document.querySelector('.big-image-6').src = src;
